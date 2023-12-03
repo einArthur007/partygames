c1ab25b91d6b0fb0db0760d2fccc6da35fb5a4e0
Arthur <arthur@kellermann.team>
2023-12-03 18:53:02 +0100

    liste für Charactere (ohne websockets)

--- lib/games/werewolf/select(werewolf).dart
+++ lib/games/werewolf/select(werewolf).dart
@@ -7,8 +7,6 @@ class Select extends StatefulWidget {
   State<Select> createState() => _SelectState();
 }
 
-<<<<<<< HEAD
-=======
 final List<String> growableList = [];
 
 List<Map<String, dynamic>> charakters = [
@@ -62,100 +60,9 @@ List<Map<String, dynamic>> charakters = [
   },
 ];
 
->>>>>>> refs/remotes/origin/main
 class _SelectState extends State<Select> {
   @override
   Widget build(BuildContext context) {
-    List characterList = [];
-
-    List<Map<String, dynamic>> charakters = [
-      {
-        'charakter': 'Doppelgänger',
-        'image': const AssetImage('assets/Werewolf/doppelgaengerin.png'),
-        'onTap': () {
-          characterList.add('doppelgaenger');
-          setState(() {});
-        },
-      },
-      {
-        'charakter': 'Werwolf',
-        'image': const AssetImage('assets/Werewolf/werewolf.png'),
-        'onTap': () {
-          characterList.add('werewolf');
-        },
-      },
-      {
-        'charakter': 'Werwolf',
-        'image': const AssetImage('assets/Werewolf/werewolf.png'),
-        'onTap': () {
-          characterList.add('werewolf');
-        },
-      },
-      {
-        'charakter': 'Günstling',
-        'image': const AssetImage('assets/Werewolf/guenstling.png'),
-        'onTap': () {
-          characterList.add('guenstling');
-        },
-      },
-      {
-        'charakter': 'Freimaurer',
-        'image': const AssetImage('assets/Werewolf/freimaurer.png'),
-        'onTap': () {
-          characterList.add('freimaurer');
-        },
-      },
-      {
-        'charakter': 'Freimaurer',
-        'image': const AssetImage('assets/Werewolf/freimaurer.png'),
-        'onTap': () {
-          characterList.add('freimaurer');
-        },
-      },
-      {
-        'charakter': 'Seherin',
-        'image': const AssetImage('assets/Werewolf/seherin.png'),
-        'onTap': () {
-          characterList.add('seherin');
-        },
-      },
-      {
-        'charakter': 'Räuber',
-        'image': const AssetImage('assets/Werewolf/raeuber.png'),
-        'onTap': () {
-          characterList.add('raeuber');
-        },
-      },
-      {
-        'charakter': 'Unruhestifterin',
-        'image': const AssetImage('assets/Werewolf/unruhestifterin.png'),
-        'onTap': () {
-          characterList.add('unruhestifterin');
-        },
-      },
-      {
-        'charakter': 'Betrunkener',
-        'image': const AssetImage('assets/Werewolf/betrunkener.png'),
-        'onTap': () {
-          characterList.add('betrunkener');
-        },
-      },
-      {
-        'charakter': 'Schlaflose',
-        'image': const AssetImage('assets/Werewolf/schlaflose.png'),
-        'onTap': () {
-          characterList.add('schlaflose');
-        },
-      },
-      {
-        'charakter': 'print',
-        'image': const AssetImage('assets/add'),
-        'onTap': () {
-          print(characterList);
-        },
-      },
-    ];
-
     return GridView.count(
       crossAxisCount: 4,
       padding: const EdgeInsets.all(50),
@@ -166,13 +73,8 @@ class _SelectState extends State<Select> {
         ...charakters.map(
           (e) => GestureDetector(
             onTap: () {
-<<<<<<< HEAD
-              e['onTap'];
-              print('foo');
-=======
               growableList.add(e['charakter'].toLowerCase());
               setState(() {});
->>>>>>> refs/remotes/origin/main
             },
             child: Column(
               children: [
@@ -186,7 +88,7 @@ class _SelectState extends State<Select> {
                 ),
                 GestureDetector(
                   onTap: () {
-                    print(characterList);
+                    print(growableList);
                   },
                   child: Container(
                     decoration:
